a23e05e5a82f6d12dba2bc51e2ce73e59fe6c86e
Yroslav12123 <varoslav797@gmail.com>
2024-11-29 13:51:36 +0200

    Update NEW.py

--- NEW.py
+++ NEW.py
@@ -2,7 +2,7 @@ smile_footprint = "\U0001F463"
 
 user_name = "David"
 user_age = 14
-результат = f"My name is {user_name}, I am {user_age} years old{smile_footprint}"
+result = f"My name is {user_name}, I am {user_age} years old{smile_footprint}"
 
-print(результат)
+print(result)
 
